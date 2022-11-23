e3656578a3f0071b5c7f26b7640cd01520058fad
NicholasZXT <1036562425@qq.com>
2022-11-23 17:48:40 +0800

    MyBatis练习

---
+++ mybatis-demos/src/main/resources/table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `students`  (
+   `id` int NOT NULL,
+   `name` varchar(255) NULL,
+   `email` varchar(255) NULL,
+   `age` int NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
