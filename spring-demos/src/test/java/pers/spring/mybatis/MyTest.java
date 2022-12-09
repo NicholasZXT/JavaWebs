046df68cc35e8fc0f69569ee412e65bb3ef49160
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2022-12-09 11:44:07 +0800

    spring整合mybatis练习

---
+++ spring-demos/src/test/java/pers/spring/mybatis/MyTest.java
@@ -0,0 +1,23 @@
+package pers.spring.mybatis;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.List;
+
+import pers.spring.mybatis.domain.Student;
+import pers.spring.mybatis.service.StudentService;
+
+public class MyTest {
+    @Test
+    public void test01(){
+        String config="mybatis/applicationContext.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
+
+        StudentService service  = (StudentService) ctx.getBean("studentService");
+        List<Student> list = service.queryStudent();
+        for(Student stu:list){
+            System.out.println("Student: " + stu);
+        }
+    }
+}
