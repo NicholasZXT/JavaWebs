0764beff4b9f72b847d04570b0b631cc486ed241
NicholasZXT <1036562425@qq.com>
2023-03-27 10:03:27 +0800

    文件组织整理

---
+++ p2-mybatis-demos/src/main/java/pers/zxt/javawebs/domain/Student.java
@@ -0,0 +1,55 @@
+package pers.zxt.javawebs.domain;
+
+/**
+ * ORM里对应于数据库的实体类
+ */
+public class Student {
+    //属性名和数据库中的列名一样
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    // 各个属性的get和set方法
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return " 学生实体的信息{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ p2-mybatis-demos/src/test/java/pers/zxt/javawebs/MyTest2.java
@@ -0,0 +1,43 @@
+package pers.zxt.javawebs;
+
+import pers.zxt.javawebs.utils.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import pers.zxt.javawebs.dao.StudentDao;
+import pers.zxt.javawebs.dao.impl.StudentDaoImpl;
+import pers.zxt.javawebs.domain.Student;
+
+public class MyTest2 {
+    // 使用 传统DAO 的方式，DAO 接口的实现类 StudentDaoImpl 的每个方法里其实做的只是配置工作，并没有太多有意义的事情
+    @Test
+    public void testSelectOne(){
+        // 接口类型  变量  =  new 接口的实现类();
+        StudentDao  dao  = new StudentDaoImpl();
+        Student student  = dao.selectStudentById(1);
+        System.out.println("通过dao执行方法获取的对象：" + student);
+    }
+
+    /**
+     * 上面使用的 StudentDao接口 的 实现类StudentDaoImpl 其实没啥大作业，mybatis可以通过反射+配置信息，完成该实现类中进行的操作.
+     * MyBatis可以代替我们来动态创建一个 DAO接口 的实现类，不需要我们手动重复实现，这就是 MyBatis 的 DAO动态代理.
+     * 不过使用DAO动态代理有一些要求：
+     *  1. mapper文件中的 namespace 必须是dao接口的全限定名称
+     *  2. mapper文件中标签的 id == dao接口中的方法名称
+     *  使用 SqlSession 对象的 getMapper() 方法即可获取 DAO接口的代理实现
+     */
+
+    // 使用 mybatis 动态代理
+    @Test
+    public void testSelectOneByProxy(){
+        //1.获取SqlSession
+        SqlSession session = MyBatisUtil.getSqlSession();
+        //2.获取dao的代理
+        StudentDao dao = session.getMapper(StudentDao.class);
+        // dao代理的实现类
+        System.out.println("dao.class: " + dao.getClass().getName());
+        Student student = dao.selectStudentById(1);
+        System.out.println("student = " + student);
+        //3.关闭SqlSession对象
+        session.close();
+    }
+}
