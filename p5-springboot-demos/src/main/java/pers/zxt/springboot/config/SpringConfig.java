895bec7a31d93a9c414e3949def3459f547b0d44
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2023-04-13 11:22:11 +0800

    spring的Java对象config使用

---
+++ p5-springboot-demos/src/main/java/pers/zxt/springboot/config/SpringConfig.java
@@ -0,0 +1,51 @@
+package pers.zxt.springboot.config;
+
+import org.springframework.context.annotation.*;
+import pers.zxt.springboot.config.domain.Student;
+
+/**
+ * @Configuration: 表示当前类是作为spring容器的配置文件使用的，相当于 bean.xml
+ * @ImportResource：表示引入 xml 定义的spring配置文件，导入其中定义的bean对象
+ * @PropertySource：表示引入 properties 配置
+ * @ComponentScan：表示spring容器的包扫描路径
+ * 下面的配置中定义了如下的bean对象：
+ *   1. springconf/applicationContext.xml 中定义了 person
+ *   2. springconf/beans.xml 中定义了 myStudent
+ *   3. 通过ComponetScan扫描到的 pers.zxt.springboot.config.domain.Teacher 对象，使用注解的方式定义，
+ *      并且从springconf/config.properties中获取值
+ */
+@Configuration
+@ImportResource(value={"classpath:springconf/applicationContext.xml", "classpath:springconf/beans.xml"})
+@PropertySource(value="classpath:springconf/config.properties")
+@ComponentScan(basePackages= "pers.zxt.springboot.config.domain")
+public class SpringConfig
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello SpringConfig!" );
+    }
+
+    /**
+     * 创建方法，方法的返回值是对象。在方法的上面加入@Bean，方法的返回值对象就注入到容器中。
+     * @Bean: 把对象注入到spring容器中。 作用相当于<bean>
+     *     位置：方法的上面
+     *     说明：@Bean,不指定对象的名称，则默认bean id 为方法名，可以使用name来指定bean对象的名称
+     */
+    @Bean
+    public Student createStudent(){
+        Student s1  = new Student();
+        s1.setName("李四");
+        s1.setAge(26);
+        s1.setSex("男");
+        return s1;
+    }
+
+    @Bean(name="myStudent3")
+    public Student anotherStudent(){
+        Student s2  = new Student();
+        s2.setName("小红");
+        s2.setAge(26);
+        s2.setSex("女");
+        return s2;
+    }
+}
---
+++ p5-springboot-demos/src/test/java/pers/zxt/springboot/SpringConfigTest.java
@@ -0,0 +1,49 @@
+package pers.zxt.springboot;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pers.zxt.springboot.config.SpringConfig;
+import pers.zxt.springboot.config.domain.*;
+
+/**
+ * 测试作为配置的SpringConfig使用
+ */
+public class SpringConfigTest
+{
+    /**
+     * 使用xml作为容器配置文件
+     */
+    @Test
+    public void test01(){
+        String config="springconf/beans.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
+        Student student = (Student) ctx.getBean("myStudent");
+        System.out.println("容器（xml配置）中的对象："+student);
+    }
+
+    /**
+     * 使用JavaConfig
+     */
+    @Test
+    public void test02(){
+        ApplicationContext ctx  = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Student student = (Student) ctx.getBean("createStudent");
+        System.out.println("使用JavaConfig创建的bean对象："+student);
+    }
+
+    @Test
+    public void test03(){
+        ApplicationContext ctx  = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Student student = (Student) ctx.getBean("myStudent3");
+        System.out.println("使用JavaConfig创建的bean对象："+student);
+    }
+
+    @Test
+    public void test05(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Teacher teacher = (Teacher) ctx.getBean("teacher");
+        System.out.println("teacher==" + teacher);
+    }
+}
