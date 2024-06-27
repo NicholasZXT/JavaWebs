abd78fff765999def8790c1c490b9d36f9b75ed7
NicholasZXT <1036562425@qq.com>
2024-06-27 17:03:50 +0800

    Java socket 编程练习

--- p7-netty-demos/src/main/java/pers/zxt/netty/net/SocketEchoClient.java
+++ p7-netty-demos/src/main/java/pers/zxt/netty/net/SocketEchoClient.java
@@ -1,19 +1,20 @@
 package pers.zxt.netty.net;
 
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.io.IOException;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class SocketEchoClient {
     public static void main(String[] args) throws IOException, InterruptedException {
         int port = 8088;
         InetAddress address = InetAddress.getByName("localhost");
-        System.out.println("address: " + address);
+        //System.out.println("address: " + address);
         for(int i=0; i < 5; i++){
-            Client client = new Client(address, port, "Hello Server, this is client " + i);
+            Client client = new Client(address, port, "Hello Server, this is client " + i, "Client-" + i);
             client.start();
             Thread.sleep(1000*2);
         }
@@ -24,21 +25,40 @@ class Client extends Thread{
     InetAddress address;
     int port;
     String message;
+    String name;
 
-    public Client (InetAddress address, int port, String message){
+    public Client (InetAddress address, int port, String message, String name){
         this.address = address;
         this.port = port;
         this.message = message;
+        this.name = name;
+        setName(name);
     }
 
     @Override
     public void run() {
         try(Socket socket = new Socket(address, port)){
-            System.out.println();
-            String msg = new Date() + "-" + message;
-            socket.getOutputStream().write(msg.getBytes(StandardCharsets.UTF_8));
+            SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            int len;
+            byte[] data = new byte[1024];
+            String msg = "[" + ft.format(new Date()) + "]-[" + name + "]-" + message;
+
+            // 向服务端发送消息
+            System.out.println(name + " is sending message ...");
+            OutputStream out = socket.getOutputStream();
+            out.write(msg.getBytes(StandardCharsets.UTF_8));
+            //socket.shutdownOutput();
+
+            // 接收服务端返回的消息
+            System.out.println(name + " waiting echo message ...");
+            InputStream inputStream = socket.getInputStream();
+            len = inputStream.read(data);
+            if (len != -1){
+                System.out.println(name + " received data: " + new String(data, 0, len));
+            }
         }catch (IOException e){
-            System.out.println();
+            e.printStackTrace();
         }
+        System.out.println(name + " is done.");
     }
 }
