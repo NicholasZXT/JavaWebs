abec95a3c3d5d2831e5f421131c247a1b801cbdd
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2024-10-14 00:10:30 +0800

    Spring复习

--- p3-spring-demos/src/main/java/pers/zxt/spring/ioc/di_annotation/Student.java
+++ p3-spring-demos/src/main/java/pers/zxt/spring/ioc/di_annotation/Student.java
@@ -3,6 +3,9 @@ package pers.zxt.spring.ioc.di_annotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 
 /**
  *
--- p3-spring-demos/src/test/java/pers/zxt/spring/ioc/IocBasicTest.java
+++ p3-spring-demos/src/test/java/pers/zxt/spring/ioc/IocBasicTest.java
@@ -21,11 +21,11 @@ public class IocBasicTest
     public void useSpring(){
         // 通过spring容器来获取对象
         // 1.指定spring配置文件: 从类路径（classpath）之下开始的路径
-        // 这里的配置文件名称为 beans.xml，但是实际中常常命名为 applicationContext.xml
-        String config= "ioc/beans.xml";
+        // 这里的配置文件名称为 start-beans.xml，但是实际中常常命名为 applicationContext.xml
+        String config= "ioc/start-beans.xml";
 
         // 2.创建容器对象， ApplicationContext 表示spring容器对象，后续通过ctx获取某个java对象
-        // 这里创建容器对象的时候，就会在将 beans.xml 配置中定义的所有对象都创建出来（同一个类可以创建多个对象）
+        // 这里创建容器对象的时候，就会在将 start-beans.xml 配置中定义的所有对象都创建出来（同一个类可以创建多个对象）
         // 这样的优点是获取对象快，但是缺点是占用内存
         ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
 
@@ -45,7 +45,7 @@ public class IocBasicTest
      */
     @Test
     public void test1(){
-        String config= "ioc/beans.xml";
+        String config= "ioc/start-beans.xml";
         ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
         SomeService service  = (SomeService) ctx.getBean("someService");
         service.doSome();
@@ -56,7 +56,7 @@ public class IocBasicTest
      */
     @Test
     public void test2(){
-        String config= "ioc/beans.xml";
+        String config= "ioc/start-beans.xml";
         ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
 
         //获取容器中定义对象的数量
@@ -76,7 +76,7 @@ public class IocBasicTest
      */
     @Test
     public void test3(){
-        String config= "ioc/beans.xml";
+        String config= "ioc/start-beans.xml";
         ApplicationContext ctx = new ClassPathXmlApplicationContext(config);
         // 获取非定义的对象
         Date date = (Date) ctx.getBean("mydate");
