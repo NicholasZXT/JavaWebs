ad839d635ac88b12c5d0b0e9ab2dfef0fe8fdcf0
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2024-10-18 18:15:54 +0800

    SpringDoc使用研究

--- p5-springboot-demos/src/main/java/pers/zxt/springboot/ssm/SwaggerOpenApiConfigBean.java
+++ p5-springboot-demos/src/main/java/pers/zxt/springboot/ssm/SwaggerOpenApiConfigBean.java
@@ -15,11 +15,26 @@ import java.util.HashMap;
  *   1. 使用 Swagger 提供的原生入口类： OpenAPI 类
  *   2. 使用 SpringDoc 封装好的入口类： GroupedOpenApi 类
  * 此种方式必须要使用 @SpringBootConfiguration 注解，否则 SpringDoc 无法获取里面的 @Bean 返回的配置对象。
+ * 默认访问URL: http://localhost:8100/swagger-ui/index.html
+ *
+ * Swagger原生的使用方式参考如下的官方文档：
+ * + [Swagger 2.X Getting started](https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Getting-started#quick-start)
+ *   快速入门
+ * + [Swagger 2.X Integration and Configuration](https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Integration-and-Configuration)
+ *   介绍了Swagger的集成和配置方式，原生的Swagger使用起来比较复杂，要通过配置对应 Servlet 启动的方式，也介绍了配置文件的使用。
+ * + [Swagger 2.X Annotations](https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations)
+ *   介绍了 Swagger 提供的一系列注解，这个比较有用。
+ * 总结如下：
+ *  1. Swagger原生使用方式要通过配置HttpServlet的方式，比较复杂
+ *  2. Swagger配置文件 openapi.yml/openapi.json 也需要自己设置读取方式（Servlet启动加载，或者代码中读取），不适用和SpringBoot集成。
+ *     **最重要的一点，使用 SpringDoc 集成时，SpringDoc 似乎并不会自动读取 resource 目录下配置文件，而是忽略它**
+ *  3. **Swagger原生提供的注解很有用，因为 SpringDoc 也是基于原生注解来配置的，并没有提供其他注解**
+ *  推荐使用 SpringDoc 提供的配置方式来对Swagger-UI进行定制，也就是在 SpringBoot 的配置文件中，以 `springdoc` 开头的配置项目。
  */
 @SpringBootConfiguration
 public class SwaggerOpenApiConfigBean {
     /***
-     * 使用 Swagger3.0 原生提供的 OpenAPI 类来构建文档配置（这里并不包含每个接口文档的内容）。
+     * 使用 Swagger3.0 原生提供的 OpenAPI 类来构建基本信息配置（这里并不包含每个接口的文档内容）。
      * 此时使用的是 io.swagger.v3.oas.models 里提供的各个 **类** 来构造 Swagger 所需要的信息，这个也是注解方式的底层实现。
      * 注意，这种返回 OpenAPI Bean 的方式是 SpringDoc 支持的，而不是 io.swagger.core.v3 原生的使用方式。
      * 原生使用方式里，还需要对 OpenAPI 对象进行封装和配置执行环境，这些工作 SpringDoc 都帮开发者配置好了。
@@ -57,17 +72,18 @@ public class SwaggerOpenApiConfigBean {
                 .info(info);      // 配置 Swagger 描述信息
         return openApi;
     }
+
     // -----------------------------------------------------------------------------------------------------------------
-    // 下面使用 SpringDoc 提供的 OpenAPI 配置会覆盖上面 Swagger 原生的配置方式
+    // 下面使用 SpringDoc 提供的 OpenAPI 配置只会在 对应Group页面 覆盖上面 Swagger 原生的配置方式
     /**
      * 使用 SpringDoc 提供的 GroupedOpenApi 来构建文档配置，它提供了API文档分组的功能。
      * 所谓分组，就是在 Swagger-UI 的接口文档页面右上方，提供了可以切换的选项卡，用于展示不同组的接口文档信息，每个组相当于一个单独的 OpenAPI 页面。
      * 此时使用的是 org.springdoc.core 里提供的 **类** 来构造 Swagger 所需要的信息。
      * GroupedOpenApi 本身不是 OpenAPI 的子类，但是它有一个 List<OpenApiCustomiser> openApiCustomisers 实例变量，其中的
-     * OpenApiCustomiser就是一个接收 OpenAPI 对象并进行各种配置的接口。
+     * OpenApiCustomiser 就是一个接收 OpenAPI 对象并进行各种配置的接口。
      * @return GroupedOpenApi 对象
      */
-    //@Bean
+    @Bean
     public GroupedOpenApi GroupedOpenApiForG1(){
         // 和上面的 OpenAPI 对象不同，这里使用了建造者模式来创建
         GroupedOpenApi groupedOpenApi = GroupedOpenApi.builder()
@@ -75,31 +91,48 @@ public class SwaggerOpenApiConfigBean {
                 .displayName("Group-1")
                 // 这里使用了lambda表达式来构造一个 OpenApiCustomiser接口的匿名实现类，在里面对 OpenAPI 配置对象设置了标题
                 // 可以将上面 customOpenAPI() 方法里的自定义 OpenAPI 页面信息的逻辑放在这里
+                // 不过需要注意的是，这里设置的只是当前 Group 页面的 OpenAPI 配置 -------- KEY
                 .addOpenApiCustomiser(openApi -> openApi.openapi("3.0.2").info(new Info().title("Title From GroupedOpenApiForG1")))
                 // 设置此分组接口要扫描的package
-                .packagesToScan("pers.zxt.springboot.ssm.controller.SwaggerController")
+                .packagesToScan("pers.zxt.springboot.ssm.controller")
                 // 设置此分组接口要排除的package
                 //.packagesToExclude("")
                 // 设置此分组接口匹配的URL路径
-                //.pathsToMatch("")
+                .pathsToMatch("/springdoc/g1/*")
                 // 设置此分组接口需要排除的URL路径
-                //.pathsToExclude("")
+                .pathsToExclude("/students/*")
                 // 下面这两个暂时不清楚是指的啥意思
                 //.producesToMatch("")
                 //.consumesToMatch("")
                 .build();
         return groupedOpenApi;
     }
-    //@Bean
+
+    @Bean
     public GroupedOpenApi GroupedOpenApiForG2(){
         GroupedOpenApi groupedOpenApi = GroupedOpenApi.builder()
-                .group("group2")   //  group的名称不能重复，这是唯一标识字段
+                .group("group2")   //  group的名称不能重复，这是唯一标识字段，并且会被用于URL中
                 .displayName("Group-2")
-                .addOpenApiCustomiser(openApi -> openApi.openapi("3.0.3").info(new Info().title("Title From GroupedOpenApiForG2")))
-                //.packagesToScan("")
+                // Group2 如果不自定义 OpenAPI，那么就会使用上面 customOpenAPI() 方法返回的 OpenAPI 对象的配置
+                //.addOpenApiCustomiser(openApi -> openApi.openapi("3.0.3").info(new Info().title("Title From GroupedOpenApiForG2")))
+                .packagesToScan("pers.zxt.springboot.ssm.controller")
                 //.packagesToExclude("")
-                //.pathsToMatch("")
-                //.pathsToExclude("")
+                .pathsToMatch("/springdoc/g2/*")
+                .pathsToExclude("/students/*")
+                .build();
+        return groupedOpenApi;
+    }
+
+    @Bean
+    public GroupedOpenApi GroupedOpenApiForBasic(){
+        // 这里先增加一个 GroupedOpenAPI，专门用于对应 SwaggerController 里的请求处理
+        GroupedOpenApi groupedOpenApi = GroupedOpenApi.builder()
+                .group("basic")
+                .displayName("Group-Basic")
+                .packagesToScan("pers.zxt.springboot.ssm.controller")
+                .pathsToMatch("/swagger/basic/*")
+                //.pathsToMatch("/springdoc/g2/*")
+                .pathsToExclude("/students/*")
                 .build();
         return groupedOpenApi;
     }
--- p5-springboot-demos/src/main/java/pers/zxt/springboot/ssm/controller/SwaggerController.java
+++ p5-springboot-demos/src/main/java/pers/zxt/springboot/ssm/controller/SwaggerController.java
@@ -1,38 +1,149 @@
 package pers.zxt.springboot.ssm.controller;
 
-import java.util.List;
 import java.util.Arrays;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.Explode;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.enums.ParameterStyle;
-import io.swagger.v3.oas.annotations.headers.Header;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
+//import io.swagger.v3.oas.annotations.parameters.RequestBody;  // 这个和上面的冲突了，只能使用全限定类名
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.enums.Explode;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+
+import pers.zxt.springboot.ssm.domain.Client;
+import pers.zxt.springboot.ssm.domain.QueryPage;
 
 /**
- * 专门用于展示 Springdoc 集成 Swagger 的 Controller
+ * 专门用于展示 Springdoc 集成 Swagger 的 Controller。
+ * SpringDoc 也是使用的 Swagger 提供的原生注解，参见如下的官方文档：
+ * [Swagger 2.X Annotations](https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations)
  */
-@RestController(value = "Swagger Controller")
-@RequestMapping(value = "/swagger")
+@RestController(value = "Swagger Basic Controller")
+@RequestMapping(value = "/swagger/basic")
 public class SwaggerController {
 
     @Operation(
-            summary = "Return Clients",
+            method = "Get",
+            summary = "Test Swagger Controller",
             description = "Some descriptions...",
             tags = {"client"}
     )
-    @GetMapping(value = "/list")
-    public List<String> listClients() {
-        return Arrays.asList("First Client", "Second Client");
+    @GetMapping(value = "/test")
+    public List<String> testCase() {
+        return Arrays.asList("First Content", "Second Content");
     }
+
+    @Operation(
+            method = "Get",
+            summary = "Test Client",
+            description = "Some descriptions...",
+            tags = {"client"}
+    )
+    //@GetMapping(value = "/test/client")
+    //@GetMapping(value = "/client/test")
+    @GetMapping(value = "/client")    // 只有这个URL才能显示在 Swagger-UI 上
+    public String testClient(){
+        Client client = new Client();
+        client.setClientId("random-id");
+        client.setClientName("random-name");
+        return client.toString();
+        //return client;
+    }
+
+    // TODO 以下接口没有问题，能正常工作，但是在 Swagger 的 Group-Basic 页面里无法看到对应的接口文档，原因未知
+    @Operation(
+            method = "Get",
+            summary = "Query one client by ID",
+            description = "Some descriptions...",
+            tags = {"client"}
+    )
+    @GetMapping(value = "/client/{id}")
+    public Client selectOneClient(
+            // 获取 GET 请求中的路径参数
+            @PathVariable(value = "id") String id
+    ){
+        System.out.println("id: " + id);
+        Client client = new Client();
+        client.setClientId(id);
+        client.setClientName("Random");
+        //return client.toString();
+        return client;
+    }
+
+    @Operation(
+            method = "Get",
+            summary = "Query all clients by query param with @RequestParam.",
+            description = "Some descriptions...",
+            tags = {"client"}
+    )
+    @GetMapping(value = "/client/list/v1")
+    public Map<String, Object> selectAllClientsV1(
+            // 获取 GET 请求中的查询参数方式一
+            @RequestParam(value = "page") int page,
+            @RequestParam(value = "size")int size
+    ){
+        System.out.println("page: " + page + ", size: " + size);
+        Map<String, Object> res = new HashMap<>();
+        res.put("page", page);
+        res.put("size", size);
+        List<Client> clients = new ArrayList<>();
+        clients.add(new Client("id-1", "client-1"));
+        clients.add(new Client("id-2", "client-2"));
+        res.put("data", clients);
+        return res;
+    }
+
+    @Operation(
+            method = "Get",
+            summary = "Query all clients by query param with Bean.",
+            description = "Some descriptions...",
+            tags = {"client"}
+    )
+    @GetMapping(value = "/client/list/v2")
+    public Map<String, Object> selectAllClientsV2(
+            // 获取 GET 请求中的查询参数方式一，使用一个Bean
+            QueryPage queryPage
+    ){
+        System.out.println(queryPage);
+        Map<String, Object> res = new HashMap<>();
+        res.put("page", queryPage.getPage());
+        res.put("size", queryPage.getSize());
+        List<Client> clients = new ArrayList<>();
+        clients.add(new Client("id-3", "client-3"));
+        clients.add(new Client("id-4", "client-4"));
+        res.put("data", clients);
+        return res;
+    }
+
+    @Operation(
+            method = "Post",
+            summary = "Add a client and echo it.",
+            description = "Some descriptions...",
+            tags = {"client"}
+    )
+    @PostMapping(value = "/client/add")
+    public Client addOneClient(
+            // 获取 POST 请求体的内容
+            @RequestBody Client client
+    ){
+        System.out.println("new client: " + client);
+        return client;
+    }
+
 }
